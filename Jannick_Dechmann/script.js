9baf28b0ea56f68517db3081774905fbd732c778
Jannick Dechmann <jannickdechmann@gmail.com>
2023-07-17 10:35:57 +0200

    Webpage edit

---
+++ Jannick_Dechmann/script.js
@@ -0,0 +1,22 @@
+function myFunction()
+{
+document.getElementById("textchange").innerHTML = "test";
+}
+
+function add() {
+  
+    result.value= (number1.value*1) + (number2.value*1);
+   }
+function subtract() {
+  
+    result.value= (number1.value*1) - (number2.value*1);
+   }
+function multiply() {
+  
+    result.value= (number1.value*1) * (number2.value*1);
+   }
+
+function divide() {
+  
+    result.value= (number1.value*1) / (number2.value*1);
+   }
